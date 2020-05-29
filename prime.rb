c6201e86139ca9ce439b1bf9141de91f8ae044b4
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 07:57:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  
+  if integer <= 1 
+   return false 
+  end
+  
+  if integer == 2 
+    return true 
+  end
+  
+  i = (2..integer-1).to_a
+  i.none? do |num|
+  integer % num == 0
+  end
+  
+end
+
+prime?(-1)
