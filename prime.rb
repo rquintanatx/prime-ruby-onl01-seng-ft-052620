76fbc3abc055b89d567015a10cd09395be08bef2
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 07:49:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  
+  if integer <= 1 
+    false 
+  end
+  
+  if integer == 2 
+    true 
+  end
+  
+  i = (3..integer).to_a
+  i.none? do |num|
+  binding.pry
+  integer % num == 0
+  end
+  
+end
+
+prime?(3)
